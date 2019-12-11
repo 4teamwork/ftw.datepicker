0c2cb0c8447ae7f275df100c5e7faae6f652c965
Matthias Osswald <m@osswald.li>
2019-12-11 14:17:18 +0100

    Add new way of building and loading resources.

---
+++ build.js
@@ -0,0 +1,9 @@
+({
+    baseUrl: "./ftw/datepicker/resources/js",
+    paths: {
+        jquery: "empty:",
+        "jquery-mousewheel": "empty:"
+    },
+    name: "main",
+    out: "./ftw/datepicker/resources/js/datepicker-compiled.js"
+})
---
+++ ftw/datepicker/resources/js/main.js
@@ -0,0 +1,1 @@
+require(['datetimepicker_widget', 'datetimepicker-2.5.18/jquery.datetimepicker.full.min.js'], function (datetimepicker_widget, datetimepicker) {});
