474caaa977d6a926ca500e52e37a9b86d82ad227
Matthias Osswald <m@osswald.li>
2019-12-11 15:34:38 +0100

    Use non minified javascript.

--- ftw/datepicker/resources/js/main.js
+++ ftw/datepicker/resources/js/main.js
@@ -1,1 +1,1 @@
-require(['datetimepicker_widget', 'datetimepicker-2.5.18/jquery.datetimepicker.full.min.js', 'jquery-mousewheel'], function (datetimepicker_widget, datetimepicker, jquery_mousewheel) {});
+require(['datetimepicker_widget', 'datetimepicker-2.5.18/jquery.datetimepicker.full.js', 'jquery-mousewheel'], function (datetimepicker_widget, datetimepicker, jquery_mousewheel) {});
